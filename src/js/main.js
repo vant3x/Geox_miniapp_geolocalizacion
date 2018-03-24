3b11f14d28a62e55dc7141ddc836ba044adadbdc
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-03-24 02:00:23 -0500

    Agregando marcadores

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,10 @@ google.maps.event.addDomListener(window, "load", function () {
         var search_input = document.getElementById('search-place');
         var autocomplete = new google.maps.places.Autocomplete(search_input);
 
+        var marker = new google.maps.Marker({
+            map: map
+        });
+
         autocomplete.bindTo("bounds", map);
 
         google.maps.event.addListener(autocomplete, "place_changed", function () {
@@ -34,6 +38,13 @@ google.maps.event.addDomListener(window, "load", function () {
                 map.setCenter(place.geometry.location);
                 map.setZoom(15);
             }
+
+            marker.setPlace({
+                placeId: place.place_id,
+                location: place.geometry.location
+            });
+
+            marker.setVisible(true);
         });
     });
 });
