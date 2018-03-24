5b1f5e57ddbd7c0480e75fedf67b2b2396512f9f
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-03-24 01:27:32 -0500

    Autocompleta y barra de busqueda

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,21 @@
 var nombre = "Alejandro";
 
 google.maps.event.addDomListener(window, "load", function () {
-    // Geolocation => Navigator
-    var user_location = new UserLocation(function () {
-        var mapOptions = {
-            zoom: 6,
-            center: {
-                lat: user_location.latitude,
-                lng: user_location.longitude
-            }
-        };
-        var mapa_element = document.getElementById('map');
-        var map = new google.maps.Map(mapa_element, mapOptions);
-    });
+  // Geolocation => Navigator
+  var user_location = new UserLocation(function () {
+    var mapOptions = {
+      zoom: 6,
+      center: {
+        lat: user_location.latitude,
+        lng: user_location.longitude
+      }
+    };
+    var mapa_element = document.getElementById('map');
+    var map = new google.maps.Map(mapa_element, mapOptions);
+
+    var search_input = document.getElementById('search-place');
+    var autocomplete = new google.maps.places.Autocomplete(search_input);
+
+    autocomplete.bindTo("bounds", map);
+  });
 });
