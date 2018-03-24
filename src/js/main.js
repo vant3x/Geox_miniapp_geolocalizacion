2f38940e331038dea8d33e021ba270cec066a387
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-03-24 01:48:24 -0500

    Centrar mapa en lugar elegido

--- src/js/main.js
+++ src/js/main.js
@@ -3,21 +3,37 @@
 var nombre = "Alejandro";
 
 google.maps.event.addDomListener(window, "load", function () {
-  // Geolocation => Navigator
-  var user_location = new UserLocation(function () {
-    var mapOptions = {
-      zoom: 6,
-      center: {
-        lat: user_location.latitude,
-        lng: user_location.longitude
-      }
-    };
-    var mapa_element = document.getElementById('map');
-    var map = new google.maps.Map(mapa_element, mapOptions);
+    // Geolocation => Navigator
+    var user_location = new UserLocation(function () {
+        var mapOptions = {
+            zoom: 6,
+            center: {
+                lat: user_location.latitude,
+                lng: user_location.longitude
+            }
+        };
+        var mapa_element = document.getElementById('map');
+        var map = new google.maps.Map(mapa_element, mapOptions);
 
-    var search_input = document.getElementById('search-place');
-    var autocomplete = new google.maps.places.Autocomplete(search_input);
+        var search_input = document.getElementById('search-place');
+        var autocomplete = new google.maps.places.Autocomplete(search_input);
 
-    autocomplete.bindTo("bounds", map);
-  });
+        autocomplete.bindTo("bounds", map);
+
+        google.maps.event.addListener(autocomplete, "place_changed", function () {
+            console.log("Cambiamos el lugar");
+
+            var place = autocomplete.getPlace();
+            // modifica el zoom para que se pueda ver todo el lugar que acabamos de seleccionar
+            // el zoom es más bajo si seleccionamos un país que si seleccionamos un lugar en especifico
+            // tienda, plaza, parque o local.
+
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(15);
+            }
+        });
+    });
 });
